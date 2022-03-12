de554b0eb3e301f30c236312c71950c3f025a761
weallbinna <nakevin004@naver.com>
2022-03-12 14:40:11 +0000

    my solution and with the better solution from someone else. i need to study hard about data type

--- index.py
+++ index.py
@@ -1,29 +1,34 @@
-a,b = map(int,input().split(" "))
-c = int(input())
+a,b,c = map(int,input().split(" "))
 
-if c>=60:
-    d = c/60   
-    e = c%60  
-    a += d
-    b += e
-    if b >= 60:
-        f = b%60
-        g = b/60
-        a += g
-        b = f
-    if a >= 24:
-        a = a%24
-        
-elif c< 60:
-    b += c
-    if b>=60:
-        g = b%60
-        f = b/60
-        a += f
-        b = g
-    if a >= 24:
-        a = a%24
+if a==b==c:
+    print(10000+a*1000)
+elif a==b!=c or a==c!=b or b==c!=a:
+    if a==b!=c:
+        print(1000+a*100)
+    elif a==c!=b:
+        print(1000+a*100)
+    elif b==c!=a:
+        print(1000+b*100)
+else:
+    if a>b and a>c:
+        print(a*100)
+    elif b>a and b>c:
+        print(b*100)
+    elif c>a and c>b:
+        print(c*100)
+       
+# better code
+# a,b,c = list(map(int, input().split()))
 
-print(int(a),int(b))
-        
-        
+# lst = [a,b,c]
+# s = list(set(lst))
+# n = len(s)
+
+# if n ==1 :
+#     print(10000+a * 1000)
+# elif n ==2 :
+#     lst.remove(s[0])
+#     lst.remove(s[1])
+#     print(1000+ lst[0]*100)
+# else:
+#     print(max(lst)*100)
