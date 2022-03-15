bf16079d6bf6e583c336bbc7518e8f9dbb6210a4
weallbinna <nakevin004@naver.com>
2022-03-15 11:23:22 +0000

    trying to solve this problem

--- index.py
+++ index.py
@@ -1,7 +1,9 @@
 import sys
+a,b = map(int,sys.stdin.readline().split(" "))
+c = []
+f = 0
+for d in range(a):
+    c.append(sys.stdin.readline().strip(" "))
+    if int(c[d])<b:
+     print(c[d], end =" ")    
 
-a = int(sys.stdin.readline())
-
-for d in range(1,a+1):
-    a,b = map(int,sys.stdin.readline().split())
-    print(f"Case #{d}: {a} + {b} = {a+b}")
