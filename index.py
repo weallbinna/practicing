225dbc20165594d41285f6a44b43fd6566ef1d48
weallbinna <nakevin004@naver.com>
2022-03-15 14:29:45 +0000

    finally solved but not fully understood the problem

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 import sys
-a,b = map(int,sys.stdin.readline().split(" "))
-c = []
-f = 0
-for d in range(a):
-    c.append(sys.stdin.readline().strip(" "))
-    if int(c[d])<b:
-     print(c[d], end =" ")    
 
+a,b = map(int,sys.stdin.readline().split(" "))
+c = sys.stdin.readline().split(" ")
+
+for d in c:
+    if int(d)<b:
+        print(d, end = " ")
+print(type(a))
