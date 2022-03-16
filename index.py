887c6a1f0165178b47d57000f16fb0139da0fde9
weallbinna <nakevin004@naver.com>
2022-03-16 14:55:16 +0000

    mistook the position of 'if' and trying another question

--- index.py
+++ index.py
@@ -1,9 +1,3 @@
-import sys
-
-a,b = map(int,sys.stdin.readline().split(" "))
-c = sys.stdin.readline().split(" ")
-
-for d in c:
-    if int(d)<b:
-        print(d, end = " ")
-print(type(a))
+while True:
+    a,b = map(int,input().split(" "))
+    print(type(a+b))
