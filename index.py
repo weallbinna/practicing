f3edded5bf6b329394aa15b472031d196d72a06e
weallbinna <nakevin004@naver.com>
2022-03-18 14:55:04 +0000

    learned executing exception and exception itself

--- index.py
+++ index.py
@@ -1,5 +1,16 @@
-import sys
-lines = sys.stdin.readlines()
-for line in lines:
-    A, B = map(int, line.split(" "))
-    print(A+B)
+class minuserror(Exception):
+    def __init__(self, msg):
+        
+        self.msg = msg
+        
+    def __str__(self):    #init 에서 정의한 값을 출력하는 것
+        return self.msg
+        
+try:
+    a = int(input("양수 입력하세요: "))
+    if a<0:
+        raise minuserror("다시 양수 입력해주세요")
+except minuserror as err:
+    print(err)
+finally :
+    print("ㅅㄱ")
