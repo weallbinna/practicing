06f3e63c7851e58a80fb7226ae70c7ffb633268f
weallbinna <nakevin004@naver.com>
2022-03-20 07:17:38 +0000

    solved another test with my solution and the shorter % smarter ver from somoneelse

--- index.py
+++ index.py
@@ -1,19 +1,12 @@
-a = list(input())
-b = []
-if len(a)<2:
-    a.insert(0,"0")
-n = 1
-b.append(a[1]) 
-c = list(str(int(a[0]) + int(a[1])))
-if len(c)<2:
-    c.insert(0,"0")
-b.append(c[1])
-while  int(a[0]) != int(b[0]) or int(a[1]) != int(b[1]) :
-    c.clear
-    c = list(str(int(b[0]) + int(b[1])))
-    if len(c)<2:
-        c.insert(0,"0")
-    d = (b[1])
-    b = list(d + c[1])
-    n +=1
-print(n)
+num = int(input())
+for n in range(num):
+    score = 0
+    s = 0
+    result = input()
+    for a in range(len(result)):
+        if result[a] == "O":
+            s+=1
+            score += s
+        else:
+            s = 0
+    print(score)
