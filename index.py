94c6d110d271cf927846d746c6c040c980ebc836
weallbinna <nakevin004@naver.com>
2022-03-23 11:44:20 +0000

    'Self Number' my solution and with better answer that i cant understand right now but i will

--- index.py
+++ index.py
@@ -1,13 +1,21 @@
-import sys
-test = int(input())
-for t in range(test):
-    b = list(map(int, sys.stdin.readline().split()))
-    avg = 0
-    for b1 in range(1,len(b)):
-        avg += b[b1]
-    avg /= b[0]
-    n = 0
-    for b1 in range(1,len(b)):
-        if b[b1]>avg:
-            n+=1
-    print("{:.3f}%".format(n/b[0]*100))
+def Selfnum(a):
+        if  a<100:
+            return a+a//10+a%10
+        elif a>=100 and a<1000:
+            b = a%100
+            return a+a//100+b//10+b%10
+        elif a>=1000 and a<10000:
+            b = a%1000
+            c = b%100
+            return a+a//1000+b//100+c//10+c%10
+S = []
+for a in range(9973):
+    S.append(Selfnum(a))
+S.sort()
+S = set(S)
+all = [n for n in range(10003)]
+for s in S:
+    all.remove(s)
+all.remove(10001)
+for num in range(len(all)):
+    print(all[num])
