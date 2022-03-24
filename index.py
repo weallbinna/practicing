ee2f2e277708591601b578ecea5a4d2bbb5845d4
weallbinna <nakevin004@naver.com>
2022-03-24 12:14:44 +0000

    mastered with using set() can apply easily now yassssssss

--- index.py
+++ index.py
@@ -1,21 +1,21 @@
-def Selfnum(a):
-        if  a<100:
-            return a+a//10+a%10
-        elif a>=100 and a<1000:
-            b = a%100
-            return a+a//100+b//10+b%10
-        elif a>=1000 and a<10000:
-            b = a%1000
-            c = b%100
-            return a+a//1000+b//100+c//10+c%10
-S = []
-for a in range(9973):
-    S.append(Selfnum(a))
-S.sort()
-S = set(S)
-all = [n for n in range(10003)]
-for s in S:
-    all.remove(s)
-all.remove(10001)
-for num in range(len(all)):
-    print(all[num])
+def Han(a):
+    a = str(a)
+    if len(a) ==1:
+        return 1
+    b = []
+    for i in range(len(a)-1):
+        b.append(int(a[i+1])-int(a[i]))
+    b = set(b)
+    if len(b) ==1:
+        return 1
+    else:
+        return 0
+
+n = 0
+i = int(input())
+for l in range(1,i+1):
+    n += Han(l)
+print(n)
+        
+        
+        
