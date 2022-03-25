79f0aa0831abaa906ddc8b59502f7904e8179535
weallbinna <nakevin004@naver.com>
2022-03-25 14:47:31 +0000

    need to study ascii

--- index.py
+++ index.py
@@ -1,21 +1,10 @@
-def Han(a):
-    a = str(a)
-    if len(a) ==1:
-        return 1
-    b = []
-    for i in range(len(a)-1):
-        b.append(int(a[i+1])-int(a[i]))
-    b = set(b)
-    if len(b) ==1:
-        return 1
-    else:
-        return 0
-
-n = 0
-i = int(input())
-for l in range(1,i+1):
-    n += Han(l)
-print(n)
-        
-        
-        
+S = input()
+r = 'abcdefghijklmnopqrstuvwxyz'
+for i in r:
+    for a in range(len(S)):
+        if i not in S:
+            print("-1", end = " ")
+            break
+        if i == S[a]:
+            print(a , end = " ")
+            break
