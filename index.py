c4d2a430c4567a8a40522d75d687fe8de6b37828
weallbinna <nakevin004@naver.com>
2022-03-26 06:29:00 +0000

    you can also remove duplicated string by set() so that you could easily count the max alphabet

--- index.py
+++ index.py
@@ -1,10 +1,27 @@
-S = input()
-r = 'abcdefghijklmnopqrstuvwxyz'
-for i in r:
-    for a in range(len(S)):
-        if i not in S:
-            print("-1", end = " ")
-            break
-        if i == S[a]:
-            print(a , end = " ")
-            break
+a = input()
+b = list(range(65,91))
+b1 = list(range(97,123))
+d = []
+d1 = []
+f = []
+for c in b :
+    d.append(a.count(chr(c)))
+for c in b1:
+    d1.append(a.count(chr(c)))
+for c in range(len(b)):
+    f.append(d[c]+d1[c])
+if f.count(max(f))>1:
+    print("?")
+else:
+    print(chr(b[f.index(max(f))]))
+
+# g = input().upper()
+
+# h = list(set(g))
+# f = []
+# for i in h:
+#     f.append(g.count(i))
+# if f.count(max(f))>1:
+#     print('?')
+# else:
+#     print(h[f.index(max(f))])
