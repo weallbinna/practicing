899a9513efece0d72a314dc0eca78457f2235282
weallbinna <nakevin004@naver.com>
2022-03-27 05:53:25 +0000

    learned replace

--- index.py
+++ index.py
@@ -1,27 +1,11 @@
-a = input()
-b = list(range(65,91))
-b1 = list(range(97,123))
-d = []
-d1 = []
-f = []
-for c in b :
-    d.append(a.count(chr(c)))
-for c in b1:
-    d1.append(a.count(chr(c)))
-for c in range(len(b)):
-    f.append(d[c]+d1[c])
-if f.count(max(f))>1:
-    print("?")
-else:
-    print(chr(b[f.index(max(f))]))
-
-# g = input().upper()
-
-# h = list(set(g))
-# f = []
-# for i in h:
-#     f.append(g.count(i))
-# if f.count(max(f))>1:
-#     print('?')
-# else:
-#     print(h[f.index(max(f))])
+a = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+b = input()
+c = 0
+for n in a:
+    if n in b:
+        d = b.count(n)
+        b = b.replace(n,' ')
+        c +=d
+b = b.replace(' ','')
+c+=len(b)
+print(c)
