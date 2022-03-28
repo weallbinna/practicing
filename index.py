add9f7d790d253315b197c835fde1beb4c407f77
weallbinna <nakevin004@naver.com>
2022-03-28 12:12:25 +0000

    now i really need math for sho

--- index.py
+++ index.py
@@ -1,20 +1,7 @@
-a = int(input())
-n = 0
-for b in range(a):
-    f = True
-    c = input()
-    for b in range(len(c)):
-        if f==False:
-            break
-        d = c.index(c[b])
-        for e in range(len(c)):
-            if c[b] == c[e]:
-                if (e-d)<=1:
-                    d = e
-                else:
-                    f =False
-                    break
-    if f == False:
-        continue
-    n+=1
-print(n)            
+a,b,c = map(int,input().split())
+if b>=c:
+    print("-1")
+else:
+    n = a//(c-b)+1
+    print(n)
+    
