94b703340ac55dd7ffe080cfe4dbcffd0d333d77
weallbinna <nakevin004@naver.com>
2022-04-04 13:53:36 +0000

    hi~

--- index.py
+++ index.py
@@ -1,7 +1,12 @@
-a,b,c = map(int,input().split())
-if b>=c:
-    print("-1")
-else:
-    n = a//(c-b)+1
-    print(n)
-    
+while True:
+    cnt = 0
+    nm = int(input())
+    if nm == 0:
+        break
+    lst = [True]*(nm*2+1)
+    for i in range(2, int(2*nm**0.5)+1):
+        if lst[i]:
+            for j in range(2*i, nm*2+1, i):
+                lst[j] = False
+    del lst[:nm+1]
+    print(lst.count(True))
