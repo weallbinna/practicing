60fec357d82e4697b8c061d1c29c6c9b5c56e749
weallbinna <nakevin004@naver.com>
2022-04-05 11:54:54 +0000

    prime number bigmac

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
-while True:
-    cnt = 0
-    nm = int(input())
-    if nm == 0:
-        break
-    lst = [True]*(nm*2+1)
-    for i in range(2, int(2*nm**0.5)+1):
-        if lst[i]:
-            for j in range(2*i, nm*2+1, i):
-                lst[j] = False
-    del lst[:nm+1]
-    print(lst.count(True))
+for re in range(int(input())):
+    even = int(input())
+    lst = [True]*(even+1)
+    for index in range(2,int(even**0.5+1)):
+        if lst[index]:
+            for index1 in range(2*index,even+1,index):
+                lst[index1] = False
+    for prime in range(even//2,even):
+        if lst[prime]:
+            if lst[even-prime]:
+                print(even-prime,prime)
+                break
